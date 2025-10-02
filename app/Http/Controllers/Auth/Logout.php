39d5b62ee13fd9e4793d576b3b957af269177ab7
Jono Oliver <jonooliver@Jonos-MacBook-Pro.local>
2025-10-02 10:55:21 +0100

    UI refresh and sign in logic

--- app/Http/Controllers/Auth/Logout.php
+++ app/Http/Controllers/Auth/Logout.php
@@ -13,9 +13,7 @@ class Logout extends Controller
      */
     public function __invoke(Request $request)
     {
-        $user = $request->user();
-
-        Auth::logout($user);
+        Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
